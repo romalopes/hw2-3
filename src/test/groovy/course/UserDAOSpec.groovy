b1a8e0448f7505b74e31480c800f75dd31d9d526
romalopes <romalopes@yahoo.com.br>
2013-10-28 01:52:32 +1100

    using grade

---
+++ src/test/groovy/course/UserDAOSpec.groovy
@@ -0,0 +1,11 @@
+package course
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anderson
+ * Date: 27/10/13
+ * Time: 21:26
+ * To change this template use File | Settings | File Templates.
+ */
+class UserDAOSpec {
+}
